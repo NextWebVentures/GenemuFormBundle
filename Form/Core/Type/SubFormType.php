81b4379095679078ef74b07eabd211fae55399dc
Adam Kusmierz <kusmierz@gmail.com>
2012-03-07 23:18:59 +0100

    Subform added

---
+++ Form/Core/Type/SubFormType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Genemu\Bundle\FormBundle\Form\Core\Type;
+
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\Extension\Core\DataMapper\PropertyPathMapper;
+
+/**
+ * A Form type that just renders the field as a p tag. This is useful for forms where certain field
+ * need to be shown but not editable.
+ *
+ * @author Adam Kuśmierz <adam@kusmierz.be>
+ */
+class SubFormType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $options = $this->getDefaultOptions($options);
+
+        foreach ($options['fields'] as $name => $config) {
+            $type = (isset($config['type']) ? $config['type'] : null);
+            $option = (isset($config['options']) ? $config['options'] : null);
+
+            $builder->add($name, $type, (array) $option);
+        }
+
+        //$builder->setAttribute('configs', $options['configs']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form)
+    {}
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultOptions(array $options)
+    {
+        $defaultOptions = array(
+            'required' => false,
+        );
+
+        if (isset($options['data_class']) && empty($options['data_class'])) {
+            $defaultOptions['empty_data'] = array();
+        }
+
+        return array_replace_recursive($defaultOptions, $options);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent(array $options)
+    {
+        return 'form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'genemu_subform';
+    }
+}
